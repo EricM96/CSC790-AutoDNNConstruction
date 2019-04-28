6703cfa1b4d012c3f70e972ca7a5371ccb35e2f0
Eric McCullough <eric96@live.missouristate.edu>
2019-04-28 15:35:19 -0500

    Move create_model into Solution Class

--- Torch/fitness.py
+++ Torch/fitness.py
@@ -11,12 +11,35 @@ import torch.nn as nn
 import torch.nn.functional as F 
 
 class Solution(nn.Module):
-    def __init__(self, moduleDict, graph):
+    def __init__(self, graph):
         super(Solution, self).__init__()
-        self.features = moduleDict
+
+        self.features = self._create_model(graph)
         self.activation = nn.ReLU() 
 
         self.activation_graph = self.populate_activation_graph(graph)
+
+    def _create_model(self, graph):
+        """
+        @params: a graph representation of a neural network
+        @returns: a nn.Module for the graph 
+        """
+        in_nodes = [key for key, value in graph.items() if len(value[0]) == 0]
+        out_nodes = [key for key, value in graph.items() if len(value[1]) == 0]
+        hidden_nodes = [key for key, value in graph.items() if len(value[0]) != 0 and len(value[1]) != 0]
+
+        moduleDict = nn.ModuleDict({}) 
+
+        for node in in_nodes:
+            moduleDict[node] = nn.Linear(1, len(graph[node][1]))
+
+        for node in hidden_nodes:
+            moduleDict[node] = nn.Linear(len(graph[node][0]), len(graph[node][1]))
+
+        for node in out_nodes:
+            moduleDict[node] = nn.Linear(len(graph[node][0]), 1)
+
+        return moduleDict
 
     def populate_activation_graph(self, graph):
         activation_graph = {}
@@ -55,31 +78,11 @@ class Solution(nn.Module):
         
 
 
-def create_model(graph):
-    """
-    @params: a graph representation of a neural network
-    @returns: a nn.Module for the graph 
-    """
-    in_nodes = [key for key, value in graph.items() if len(value[0]) == 0]
-    out_nodes = [key for key, value in graph.items() if len(value[1]) == 0]
-    hidden_nodes = [key for key, value in graph.items() if len(value[0]) != 0 and len(value[1]) != 0]
 
-    moduleDict = nn.ModuleDict({}) 
-
-    for node in in_nodes:
-        moduleDict[node] = nn.Linear(1, len(graph[node][1]))
-
-    for node in hidden_nodes:
-        moduleDict[node] = nn.Linear(len(graph[node][0]), len(graph[node][1]))
-
-    for node in out_nodes:
-        moduleDict[node] = nn.Linear(len(graph[node][0]), 1)
-
-    return Solution(moduleDict, graph) 
 
 if __name__ == "__main__":
     # graph = json.loads(sys.argv[1])
     graph = {'1': [[], ['4']], '3': [[], ['5']], '2': [[], ['4', '5']], '5': [['3', '2', '4'], []], '4': [['1', '2'], ['5']]}
     print(graph) 
-    model = create_model(graph) 
+    model = Solution(graph)
     model.feed_forward(np.random.rand(1, 3))
