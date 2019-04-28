ca118febbadaefdc7741ce0c69dfbe83cea159f6
Eric McCullough <eric96@live.missouristate.edu>
2019-04-28 12:10:16 -0500

    add comments and docstrings to fitness.py

--- Torch/fitness.py
+++ Torch/fitness.py
@@ -1,3 +1,9 @@
+"""
+@Author: Eric McCullough
+@Description: this module takes a graph representation of a solution generated
+              by the GA code, creates a PyTorch neural network from those features
+              and determines fitness by running it on training and validation data
+"""
 import sys, json
 import torch.nn as nn
 import torch.nn.functional as F 
@@ -8,13 +14,18 @@ class Solution(nn.Module):
         self.features = moduleDict
 
 def id_features(graph):
+    """
+    @params: a graph representation of a neural network
+    @returns: a nn.Module for the graph 
+    """
     in_nodes = [key for key, value in graph.items() if len(value[0]) == 0]
     out_nodes = [key for key, value in graph.items() if len(value[1]) == 0]
-    hidden_nodes = [key for key, value in graph.items() if len(value[0]) != 0 and len(value[1]) != 0]
+    hidden_nodes = [key for key, value in graph.items() if len(value[0]) != 0 
+                                                        and len(value[1]) != 0]
 
-    print("Input nodes: ", in_nodes)
-    print("Hidden nodes", hidden_nodes)
-    print("out_nodes", out_nodes)
+    # print("Input nodes: ", in_nodes)
+    # print("Hidden nodes", hidden_nodes)
+    # print("out_nodes", out_nodes)
 
     moduleDict = nn.ModuleDict({}) 
 
