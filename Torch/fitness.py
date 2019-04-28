1bdb799cde3f3a3b7601ef6bd3ef9e4ec1113308
Eric McCullough <eric96@live.missouristate.edu>
2019-04-28 15:29:32 -0500

    Add function for creating activation graph and populating inputs

--- Torch/fitness.py
+++ Torch/fitness.py
@@ -5,28 +5,64 @@
               and determines fitness by running it on training and validation data
 """
 import sys, json
+import numpy as np
+from torch import tensor
 import torch.nn as nn
 import torch.nn.functional as F 
 
 class Solution(nn.Module):
-    def __init__(self, moduleDict):
+    def __init__(self, moduleDict, graph):
         super(Solution, self).__init__()
         self.features = moduleDict
         self.activation = nn.ReLU() 
 
-def id_features(graph):
+        self.activation_graph = self.populate_activation_graph(graph)
+
+    def populate_activation_graph(self, graph):
+        activation_graph = {}
+
+        for key in graph.keys():
+            input_len = len(graph[key][0])
+            output_len = len(graph[key][1])
+
+            if input_len == 0:
+                node_type = "input"
+            elif output_len == 0:
+                node_type = "output"
+            else:
+                node_type = "hidden"
+
+            activation_graph[key] = {'input tensor' : 
+                np.zeros(input_len) if input_len > 0 else np.array([0.]),
+                'node type' : node_type,
+                'output nodes': graph[key][1],
+                'dependencies': graph[key][0]}
+
+        return activation_graph
+
+    def feed_forward(self, X):
+        print(self.activation_graph)
+        print()
+        self._populate_inputs(X)
+        print(self.activation_graph)
+
+    def _populate_inputs(self, X):
+        i = 0
+        for key, value in self.activation_graph.items():
+            if value['node type'] == 'input':
+                self.activation_graph[key]['input tensor'][0] = X[0][i]
+                i += 1
+        
+
+
+def create_model(graph):
     """
     @params: a graph representation of a neural network
     @returns: a nn.Module for the graph 
     """
     in_nodes = [key for key, value in graph.items() if len(value[0]) == 0]
     out_nodes = [key for key, value in graph.items() if len(value[1]) == 0]
-    hidden_nodes = [key for key, value in graph.items() if len(value[0]) != 0 
-                                                        and len(value[1]) != 0]
-
-    # print("Input nodes: ", in_nodes)
-    # print("Hidden nodes", hidden_nodes)
-    # print("out_nodes", out_nodes)
+    hidden_nodes = [key for key, value in graph.items() if len(value[0]) != 0 and len(value[1]) != 0]
 
     moduleDict = nn.ModuleDict({}) 
 
@@ -39,10 +75,11 @@ def id_features(graph):
     for node in out_nodes:
         moduleDict[node] = nn.Linear(len(graph[node][0]), 1)
 
-    solution = Solution(moduleDict) 
-    print(solution)
+    return Solution(moduleDict, graph) 
 
 if __name__ == "__main__":
-    graph = json.loads(sys.argv[1])
+    # graph = json.loads(sys.argv[1])
+    graph = {'1': [[], ['4']], '3': [[], ['5']], '2': [[], ['4', '5']], '5': [['3', '2', '4'], []], '4': [['1', '2'], ['5']]}
     print(graph) 
-    id_features(graph) 
+    model = create_model(graph) 
+    model.feed_forward(np.random.rand(1, 3))
