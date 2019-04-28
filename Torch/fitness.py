93d9c1859b78fa4596619370a6f69aa45dcbbbf5
Eric McCullough <eric96@live.missouristate.edu>
2019-04-28 11:55:16 -0500

    Construct hidden and output network features

--- Torch/fitness.py
+++ Torch/fitness.py
@@ -5,11 +5,22 @@ import torch.nn.functional as F
 def id_features(graph):
     in_nodes = [key for key, value in graph.items() if len(value[0]) == 0]
     out_nodes = [key for key, value in graph.items() if len(value[1]) == 0]
+    hidden_nodes = [key for key, value in graph.items() if len(value[0]) != 0 and len(value[1]) != 0]
+
+    print("Input nodes: ", in_nodes)
+    print("Hidden nodes", hidden_nodes)
+    print("out_nodes", out_nodes)
 
     moduleDict = {} 
 
     for node in in_nodes:
         moduleDict[node] = nn.Linear(1, len(graph[node][1]))
+
+    for node in hidden_nodes:
+        moduleDict[node] = nn.Linear(len(graph[node][0]), len(graph[node][1]))
+
+    for node in out_nodes:
+        moduleDict[node] = nn.Linear(len(graph[node][0]), 1)
 
     print(moduleDict) 
 
