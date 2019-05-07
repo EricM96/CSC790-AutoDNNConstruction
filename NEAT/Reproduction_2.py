fa805bb4f8927637a3365c102d3bb46592183c8f
cupofcalculus <cupofcalculus@gmail.com>
2019-05-07 15:27:59 -0500

    Update Reproduction_2.py

--- NEAT/Reproduction_2.py
+++ NEAT/Reproduction_2.py
@@ -11,7 +11,7 @@ def addLayer(individual):
     if coinflip < adaptiveChance:
         cut = random.randint(1,len(individual.layers)-1)
         newLayer = individual.layers[:cut]
-        newLayer.append(2)
+        newLayer.append(5)
         for item in individual.layers[cut:]:
             newLayer.append(item)
         return newLayer
